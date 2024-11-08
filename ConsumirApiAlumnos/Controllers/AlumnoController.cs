9484a62cfa2734904af24d72cccf3828f62b4000
AnGH56 <112748@alumnouninter.mx>
2024-11-07 19:07:03 -0600

    Agregar archivos de proyecto.

---
+++ ConsumirApiAlumnos/Controllers/AlumnoController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsumirApiAlumnos.Models;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ConsumirApiAlumnos.Controllers
+{
+    internal class AlumnoController
+    {
+        static readonly HttpClient client = new HttpClient()
+        {
+            BaseAddress = new Uri("http://localhost:5170/")
+        };
+
+        public async Task ObtenerAlumnosAsync() 
+        {
+            
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.GetAsync("AlumnosApi/Alumnos");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var al = JsonConvert.DeserializeObject<List<Alumno>>(await response.Content.ReadAsStringAsync());
+                foreach (var alumnos in al)
+                {
+                    Console.WriteLine($"ID: {alumnos.Id} \n Nombre: {alumnos.Nombre} \n Edad:{alumnos.Edad} \n " +
+                        $"Fecha de Nacimiento: {alumnos.FechaNacimiento}");
+                    Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - -");
+                }
+            }
+        }
+
+        public async Task EliminarAlumnosAsync(int Id) 
+        {
+            //client.DefaultRequestHeaders.Accept.Clear();
+            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            Console.WriteLine($"ID:{Id}");
+            HttpResponseMessage response = await client.DeleteAsync("AlumnosApi/Alumnos/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine($"Alumno con ID {Id} eliminado correctamente.");
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                Console.WriteLine($"Alumno con ID {Id} no encontrado.");
+            }
+            else
+            {
+                Console.WriteLine($"Error al eliminar el alumno con ID {Id}: {response.ReasonPhrase}");
+            }
+
+        }
+    }
+}
---
+++ ConsumirApiAlumnos/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using ConsumirApiAlumnos.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumirApiAlumnos
+{
+    internal class Program
+    {
+        public enum Menu{VerAlumnos=1, AgregarAlumno, ActualizarAlumno,EliminarAlumno,Salir }
+        static void Main(string[] args)
+        {
+            AlumnoController alctrl = new AlumnoController();
+
+            while (true)
+            {
+                Console.WriteLine("- - B I E N V E N I D O - -");
+                Console.WriteLine("¿Qué deseas hacer?");
+
+                switch (Seleccion())
+                {
+                    case Menu.VerAlumnos:
+                        Console.WriteLine("1) Ver Alumnos");
+                        alctrl.ObtenerAlumnosAsync().Wait();
+                        break;
+                    case Menu.AgregarAlumno:
+                        break;
+                    case Menu.ActualizarAlumno:
+                        break;
+                    case Menu.EliminarAlumno:
+                        Console.WriteLine("4) Eliminar Alumno");
+                        Console.WriteLine("Inserta el ID del alumno que deseas eliminar:");
+                        int id = Convert.ToInt32(Console.ReadLine());
+                        alctrl.EliminarAlumnosAsync(id).Wait();
+                        break;
+                    case Menu.Salir:
+                        break;
+                    default:
+                        break;
+                }
+                Console.ReadKey();
+            }
+        }
+
+        static Menu Seleccion() 
+        {
+            Console.WriteLine("1) Ver Alumnos");
+            Console.WriteLine("2) Insertar Alumno");
+            Console.WriteLine("3) Actualizar Alumno");
+            Console.WriteLine("4) Eliminar Alumno");
+            Console.WriteLine("5) Salir");
+
+            Menu opc = (Menu)Convert.ToInt32(Console.ReadLine());
+
+            return opc;
+        }
+    }
+}
